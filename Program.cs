d0c729311759e71ca58dcdad70aca2e7e72ae4a9
Den <Polk300i@mail.ru>
2023-05-24 11:10:59 +0300

    Исправил ошибку в команде eles в програме котороя проверяет является ли первое число квадратом второго числа

--- Program.cs
+++ Program.cs
@@ -25,18 +25,25 @@ a = 9, b = -3 -> да
 a = -3 b = 9 -> нет
 */
 
-
+/*
 Console.Write("Видите первое число: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Видите второе число: ");
 int seondNumbers = Convert.ToInt32(Console.ReadLine());
 
-if (firstNumber == seondNumbers * seondNumbers);
+if (firstNumber == seondNumbers * seondNumbers)
 {
-    Console.WriteLine("Число "+ firstNumber + "Является кводратом числа " + seondNumbers);
+    Console.WriteLine("Число " + firstNumber + " является кводратом числа " + seondNumbers);
 }
 else
 {
-    Console.WriteLine("Число "+ firstNumber + "Не является кводратом числа" + seondNumbers);
+    Console.WriteLine("Число " + firstNumber + " не является кводратом числа " + seondNumbers);
 }
+*/
+
+
+//5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
