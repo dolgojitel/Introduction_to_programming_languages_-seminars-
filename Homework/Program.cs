26599300758aec1ad56da4828d283897f582e3bb
Den <Polk300i@mail.ru>
2023-05-25 01:00:11 +0300

    4 задача котороя принимает на вход три числа и выдает максимальное из трех чисел

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -20,3 +20,23 @@ else
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+/*
+Console.Write("Видите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Видите второе число: ");
+int seondNumbers = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Видите третее число: ");
+int thirdNumbers = Convert.ToInt32(Console.ReadLine());
+
+int maxNamber = firstNumber;
+
+
+if (firstNumber > maxNamber) maxNamber = firstNumber;
+if (seondNumbers > maxNamber) maxNamber = seondNumbers;
+if (thirdNumbers > maxNamber) maxNamber = thirdNumbers;
+      Console.WriteLine(maxNamber);
+
+*/
+
